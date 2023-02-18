7e1ceebae4febbed4d9791120e5f6ccf10fa5b94
Kaladin-04 <77846711+Kaladin-04@users.noreply.github.com>
2023-02-18 08:57:39 +0530

    Update hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -6,5 +6,6 @@ int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
   cout << "I have successfully built and run\n";
+  cout << "Hello my name is Nischal R Bhat";
   return 0;
 }
