973344afe91be47d233f95af3d496b7004814987
Kaladin-04 <77846711+Kaladin-04@users.noreply.github.com>
2023-02-18 13:17:50 +0530

    Update working.cpp

--- main/working.cpp
+++ main/working.cpp
@@ -4,6 +4,6 @@ using namespace std;
 int main()
 {
   cout<<"Hello, my name is Nischal!"<<endl;
-  cout<<"This program is for Jenkins pipeline"<<endl;
+  cout<<"This program is for Jenkins pipeline<<endl; //Not closed the double quotation for cout
   return 0;
 }
